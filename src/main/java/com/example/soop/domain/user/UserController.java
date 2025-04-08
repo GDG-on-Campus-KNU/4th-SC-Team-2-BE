7499dc2fffc0841ac3d320858eb3bc7a11338e20
donghyuun <bomul0524@gmail.com>
2025-04-09 00:51:51 +0900

    feat: 엑세ì엑세스 토큰 재발급

--- src/main/java/com/example/soop/domain/user/UserController.java
+++ src/main/java/com/example/soop/domain/user/UserController.java
@@ -2,25 +2,29 @@ package com.example.soop.domain.user;
 
 import com.example.soop.domain.user.req.LoginRequest;
 import com.example.soop.domain.user.req.SignupRequest;
+import com.example.soop.domain.user.res.RefreshTokenResponse;
 import com.example.soop.global.format.ApiResponse;
+import com.example.soop.global.jwt.JwtProvider;
 import com.example.soop.global.jwt.TokenResponse;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/api/v1")
+@RequestMapping("/api/v1/users")
 @RequiredArgsConstructor
 public class UserController {
 
     private final UserService userService;
+    private final JwtProvider jwtProvider;
 
     @Operation(summary = "회원 가입")
-    @PostMapping("/auth/signup")
+    @PostMapping("/signup")
     @ApiResponses({
         @io.swagger.v3.oas.annotations.responses.ApiResponse(responseCode = "USER401", description = "이미 존재하는 유저입니다."),
     })
@@ -30,7 +34,7 @@ public class UserController {
     }
 
     @Operation(summary = "로그인")
-    @PostMapping("/auth/login")
+    @PostMapping("/login")
     @ApiResponses({
         @io.swagger.v3.oas.annotations.responses.ApiResponse(responseCode = "USER402", description = "존재하지 않는 유저입니다."),
     })
@@ -38,4 +42,20 @@ public class UserController {
         TokenResponse tokenResponse = userService.login(request);
         return ApiResponse.createSuccessWithData(tokenResponse,"로그인에 성공했습니다.");
     }
+
+    @PostMapping("/refresh")
+    @Operation(summary = "엑세스 토큰 재발급")
+    @ApiResponses({
+        @io.swagger.v3.oas.annotations.responses.ApiResponse(responseCode = "REFRESH401", description = "리프레시 토큰이 DB에 존재하지 않습니다."),
+        @io.swagger.v3.oas.annotations.responses.ApiResponse(responseCode = "REFRESH402", description = "해당 유저에 등록된 리프레시과 일치하지 않습니다."),
+        @io.swagger.v3.oas.annotations.responses.ApiResponse(responseCode = "REFRESH403", description = "리프레시 토큰이 만료되었습니다."),
+    })
+    public ApiResponse<RefreshTokenResponse> recreateAccessToken(
+        @RequestHeader("Refresh-Token") String refreshToken
+    ) {
+        RefreshTokenResponse refreshTokenResponse = jwtProvider.recreateAccessToken(refreshToken);
+        return ApiResponse.createSuccessWithData(refreshTokenResponse,
+            "엑세스 토큰이 재발급 되었습니다.");
+    }
+
 }
